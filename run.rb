718f73c3e31c86da9d24d0160c4a31f6d343fa73
Yuri Smirnov <tycoooon@gmail.com>
2025-01-26 16:13:51 +0300

    initial commit

---
+++ run.rb
@@ -0,0 +1,45 @@
+require "csv"
+
+Result = Struct.new(:id, :net, :nicknames, :payouts)
+Payout = Struct.new(:target_id, :amount)
+
+path = ARGV.shift or abort "No file path provided"
+
+results = {}
+
+CSV.foreach(path, headers: true) do |row|
+  id = row.fetch("player_id")
+  net = row.fetch("net").to_f
+  nickname = row.fetch("player_nickname")
+  result = results[id] || Result.new(id, 0, [], [])
+  result.nicknames = [*result.nicknames, nickname].sort.uniq
+  result.net += net
+  next if net.zero?
+  results[id] = result
+end
+
+winners, losers = results.values.partition { |x| x.net.positive? }
+
+losers.sort_by(&:net).each do |loser|
+  winners.sort_by(&:net).each do |winner|
+    amount = [loser.net.abs, winner.net].min
+    next if amount.zero?
+    payout = Payout.new(winner.id, amount)
+    winner.net -= amount
+    loser.net += amount
+    loser.payouts << payout
+  end
+end
+
+show_user = -> (user) do
+  user.nicknames.first
+end
+
+losers.sort_by(&:net).each do |loser|
+  payouts = loser.payouts.map do |payout|
+    target = results.fetch(payout.target_id)
+    "#{payout.amount} -> #{show_user.call(target)}"
+  end
+
+  puts "#{show_user.call(loser)}: #{payouts.join(", ")}"
+end
